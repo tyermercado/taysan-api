d3689567633f8279cdf717c8633269e0c471dd0d
Tyer Leinster Mercado <leinster.tyer12@gmail.com>
2024-06-12 19:32:29 +0800

    initial commit

---
+++ src/main/java/com/taysanhs/taysan_api/TaysanApiApplication.java
@@ -0,0 +1,13 @@
+package com.taysanhs.taysan_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaysanApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TaysanApiApplication.class, args);
+	}
+
+}
